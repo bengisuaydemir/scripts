f987a51be3fa5e0e3e1ef613070a3238616fef2a
İpek Bengisu Aydemir <56517587+bengisuaydemir@users.noreply.github.com>
2022-01-09 19:02:21 +0300

    Update delete_files.sh

--- delete_files.sh
+++ delete_files.sh
@@ -1,3 +1,3 @@
 rm -rf /home/ipek/test/files/* #delete all files under the files folder
-rm -rf /home/ipek/test/test1/*txt #delete all txt files under the files folder
+rm -rf /home/ipek/test/test1/*txt #delete all txt files under the test1 folder
 rm -rf /home/ipek/test/test2/*pdf  #delete all pdf files under the test2 folder
